6b978160341b8547a3b6e8cfe32c0896f85eb5de
Vaidu Muñoz <115045666+vaidugm@users.noreply.github.com>
2023-06-07 23:08:01 +0000

    test

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,1 +1,18 @@
-MTAwODg5NTc5NTkzMzc1MzQ3NQ.GFLf_5.bLWLC30TfXknNSy8M-kF3_ONkA3
+const { EmbedBuilder } = require('discord.js');
+
+const embed = new EmbedBuilder();
+
+module.exports = {
+    name: 'avatar',
+    aliases: [],
+    description: 'Para ver el avatar de los usuarios',
+    run: async (client, message, args) => {
+
+        const user = message.mentions.users.first() || message.author;
+        const color = await message.guild?.members.fetch(message.client.user.id).then(color => color.displayHexColor) || '#000000';
+        embed.setColor(color);
+        embed.setTitle('Avatar de $(user.username)');
+        embed.setImage(user.displayAvatarURL({ dynamic: true, size: 1024}));
+        return message.channel.send({ embeds: [embed] });   
+    }
+};
